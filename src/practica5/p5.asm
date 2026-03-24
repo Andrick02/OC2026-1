d73bcfb2e0791362dff3144d7396a1176147571f
Andrick02 <andrick.amaya@uabc.edu.mx>
2026-03-24 21:56:27 +0000

    salvar cambios

--- src/practica5/p5.asm
+++ src/practica5/p5.asm
@@ -11,12 +11,15 @@ _start:
 	mov edx, msg2		; edx = dirección de la cadena msg
 	call puts			; imprime cadena msg terminada en valor nulo (0)
 
+	mov edx, msg1
+	call puts
+
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
 section	.data
     msg1	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
-	msg2	db  'Cadena antes ed modificar: ',0
+	msg2	db  'Cadena antes de modificar: ',0
 
 
 
